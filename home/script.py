1c322e8898137e30e451879f7ff2bb0f7d146beb
Josue-JP <josueportillod07@gmail.com>
2025-07-31 14:09:38 -0400

    Update script.py

--- home/script.py
+++ home/script.py
@@ -7,5 +7,7 @@ def get_file_contents(file):
 
 
 print(get_file_contents("bashrc"))
+print("<<<DELIMITER>>>")
 print(get_file_contents("vimrc"))
+print("<<<DELIMITER>>>")
 print(get_file_contents("tmux.conf"))
