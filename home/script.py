a14260f8c781173f415c6331c5b0ffe7cd3d8611
Josue-JP <josueportillod07@gmail.com>
2025-07-31 13:53:17 -0400

    Create script.py

---
+++ home/script.py
@@ -0,0 +1,9 @@
+def get_file_contents(file):
+    with open(file, "r") as f:
+        content = f.readlines()
+
+    return content
+
+
+
+print(get_file_contents(bashrc), get_file_contents(vimrc), get_file_contents(tmux.conf))
