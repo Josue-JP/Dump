472c3be41b0fcb809473b63e43b69630c96fcc9b
Josue-JP <josueportillod07@gmail.com>
2025-07-31 14:14:22 -0400

    Update script.py

--- home/script.py
+++ home/script.py
@@ -1,13 +1,11 @@
+import sys
+
 def get_file_contents(file):
     with open(file, "r") as f:
         content = f.read()
 
     return content
 
+file = sys.argv[1]
 
-
-print(get_file_contents("bashrc"))
-print("<<<DELIMITER>>>")
-print(get_file_contents("vimrc"))
-print("<<<DELIMITER>>>")
-print(get_file_contents("tmux.conf"))
+print(get_file_contents(file))
